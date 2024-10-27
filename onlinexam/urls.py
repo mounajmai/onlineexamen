585c49a58484bc402bd6f06bff43f318602f2ae4
mouna jmai <mouna.jmai@gmail.com>
2024-10-27 14:37:13 +0100

    first

---
+++ onlinexam/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path,include
+from django.contrib import admin
+from exam import views
+from django.contrib.auth.views import LogoutView,LoginView
+urlpatterns = [
+   
+    path('student/',include('student.urls')),
+    path('teacher/',include('teacher.urls')),
+    
+    
+
+
+    path('',views.home_view,name=''),
+    path('logout', LogoutView.as_view(template_name='exam/logout.html'),name='logout'),
+    path('contactus', views.contactus_view),
+    path('afterlogin', views.afterlogin_view,name='afterlogin'),
+
+ 
+
+]
---
+++ student/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from student import views
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+path('studentclick', views.studentclick_view),
+path('studentlogin', LoginView.as_view(template_name='student/studentlogin.html'),name='studentlogin'),
+path('studentsignup', views.student_signup_view,name='studentsignup'),
+
+]
